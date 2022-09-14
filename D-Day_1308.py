99a16ca59fb60feb7ff3553d253701c793822f64
hwi1018 <hwi101810@gmail.com>
2022-09-14 23:02:51 +0900

    from datetime import datetime 을 통해 리스트에 넣은 year, month, day 데이터를 날짜로 쉽게 변환 가능 (나중날짜 - 현재날짜).days

---
+++ D-Day_1308.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+
+def GetYear1000Days(start_year):
+    day = 0  
+    for y in range(start_year, start_year+1000):
+        if(y % 400==0):
+            day += 366
+        elif(y % 100 ==0):
+            day += 365
+        elif(y % 4 ==0):
+            day +=366
+        else:
+            day +=365
+    return day
+
+today = list(map(int, input().split()))
+dday = list(map(int, input().split()))
+
+today_date = datetime(today[0], today[1], today[2])
+dday_date = datetime(dday[0], dday[1], dday[2])
+
+ddays = (dday_date - today_date).days
+
+year1000Days = GetYear1000Days(today[0])
+
+if (year1000Days <= ddays):
+    print('gg')
+else:
+    print("D-{0}".format(ddays))
